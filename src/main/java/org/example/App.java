e42d60a80768edb446a2224f058965a0fc06aba1
Ravi Kumar <kumarravi@vmware.com>
2020-02-23 17:34:47 +0530

    Added Code to Connect and display database product name and version

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,38 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    private static final String url = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String username = "postgres";
+    private static final String password = "Welcome123";
+
+    public Connection connect() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to Postgres SQL Successfully");
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println("Database name is : " + metaData.getDatabaseProductName());
+            System.out.println("Database version is : " + metaData.getDatabaseProductVersion());
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return connection;
+
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.connect();
+
     }
 }
